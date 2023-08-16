cd7365e7095ed75b08bfee6bbe6625309b05f02d
rin1024 <sorauta.net@gmail.com>
2023-08-16 15:52:25 +0900

    使用するライブラリを変更

--- examples/smoothingReadings_hx711Test/smoothingReadings_hx711Test.ino
+++ examples/smoothingReadings_hx711Test/smoothingReadings_hx711Test.ino
@@ -1,5 +1,5 @@
 #include <SmoothingReadings.h>
-#include "HX711.h"
+#include <HX711_ADC.h>
 
 #define __M_COMPILE_DATE__ __DATE__ " " __TIME__
 #define __M_FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
@@ -12,12 +12,15 @@ char messageValue[NUM_BUF];
 int messageValueIndex = 0;
 
 /////////////// sensor
+#define STABILIZING_TIME 2000 // unsigned long
+#define CALIBRATION_FACTOR 696.0
 #define NUM_READINGS 3
 
 #define LOADCELL_DOUT_PIN A4
 #define LOADCELL_SCK_PIN  A5
 
-HX711 scale;
+HX711_ADC scale(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+
 SmoothingReadings sensorValues;
 
 ////////////////////////////////////////
@@ -36,14 +39,25 @@ void setup() {
   sensorValues.enableDebug(DEBUG_TYPE_PLOT);
   sensorValues.showPlotLabel();
 
-  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
-  scale.tare();
+  // init HX711
+  scale.begin();
+  scale.start(STABILIZING_TIME, true);
+  if (scale.getTareTimeoutFlag()) {
+    Serial.println("Timeout, check MCU>HX711 wiring and pin designations");
+    while (1);
+  }
+  else {
+    scale.setCalFactor(CALIBRATION_FACTOR);
+  }
 }
 
 void loop() {
-  if (scale.is_ready()) {
-    long reading = scale.read();
-    boolean updated = sensorValues.update(reading);
+  if (scale.update()) {
+    long scaleLongVal = long(scale.getData() * 100.0);
+    
+    if (sensorValues.update(scaleLongVal)) {
+      // something todo
+    }
 
     // readings一覧を表示
     //sensorValues.dumpReadings();
